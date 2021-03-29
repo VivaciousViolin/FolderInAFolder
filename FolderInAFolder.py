41e352d129cdacf7748b5ab1919bfa6f6cc52940
Alex <81312945+VivaciousViolin@users.noreply.github.com>
2021-03-29 13:51:52 -0400

    Update FolderInAFolder.py
    do

--- FolderInAFolder.py
+++ FolderInAFolder.py
@@ -8,9 +8,11 @@ directory = "Homework"
 #parent directory path
 parent_dir = "D:/Homework/"
 
+file_number = 1
 
 def make_and_add_path():
     global parent_dir
+    
     # Path
     path = os.path.join(parent_dir, directory)
     
@@ -18,11 +20,22 @@ def make_and_add_path():
     os.mkdir(path)
     print("Directory in " + path + " created")
     
+    #add path and print name
     #add path
     extra_file_path = "Homework/"
     parent_dir += extra_file_path
+    
+    #file number
+    global file_number
+    file_add = 1
+    file_number += file_add
+    
+    #print
     print(parent_dir)
+    print(file_number)
+    return file_number
     return parent_dir
+    
 
 
 for loop in range(10000):
