c1574a7c1db30881265bd01450e260934e1a4099
Alex <81312945+VivaciousViolin@users.noreply.github.com>
2021-03-29 12:07:18 -0400

    Update FolderInAFolder.py

--- FolderInAFolder.py
+++ FolderInAFolder.py
@@ -7,7 +7,36 @@ directory = "Homework"
  
 #parent directory path
 parent_dir = "D:/Homework/"
+
+
+def make_and_add_path():
+    global parent_dir
+    # Path
+    path = os.path.join(parent_dir, directory)
     
+    #make the directory
+    os.mkdir(path)
+    print("Directory in " + path + " created")
+    
+    #add path
+    extra_file_path = "Homework/"
+    parent_dir += extra_file_path
+    print(parent_dir)
+    return parent_dir
+
+
+make_and_add_path()
+make_and_add_path()
+make_and_add_path()
+make_and_add_path()
+make_and_add_path()
+make_and_add_path()
+
+time.sleep(5)
+
+
+"""
+#define D:/homework and make additional directory in it
 def define_and_make_directory(directory, parent_dir):
     # Path
     path = os.path.join(parent_dir, directory)
@@ -16,16 +45,34 @@ def define_and_make_directory(directory, parent_dir):
     os.mkdir(path)
     print("Directory in " + path + " created")
     
+#add Homework/ to the end of D:/Homework/
 def edit_directory():
-    # Using join()
-    # adding one string to another
-    edited_parent_dir = "".join((parent_dir, "Homework/"))
-    print(edited_parent_dir)
+    extra_file_path = "Homework/"
+    global final_dir
+    final_dir = parent_dir + extra_file_path
+    print(final_dir + "1")
+
+
+#make another directory with final_dir
+def continuation(directory, final_dir):
+    # Path
+    path = os.path.join(final_dir, directory)
     
-
-
+    #make the directory
+    os.mkdir(path)
+    print("Directory in " + path + " created")
+    
+    
 define_and_make_directory(directory, parent_dir)
 edit_directory()
+continuation(directory, final_dir)
 
-print(parent_dir)
-time.sleep(3)
+
+
+#make this step loop
+#i = 1
+#while i < 6:
+  #print(i)
+  #i += 1
+
+"""
